975b9d314834cf74023cfe747166348c505bb5c9
Stephanomejia17 <stephano.mejia@outlook.es>
2024-04-07 16:38:32 -0500

    Node and LinkedList Classes

---
+++ classes/Classes.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self, value=None, next_node=None) -> None:
+        self.value = value
+        self.next_node = next_node
+
+    def getNext(self):
+        return self.next_node
+
+    def getValue(self):
+        return self.value
+
+    def setNext(self, next_node) -> None:
+        self.next_node = next_node
+
+    def setValue(self, value) -> None:
+        self.value = value
+
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.head: Node = None
+        self.size = 0
+
+    def traverse(self):  # Imprimir la lista
+        while self.head != None:
+            print(self.head.getValue())
+            self.head = self.head.getNext()
+
+    def append(self, node, e):  # Agregar nodos al final de la lista
+        if self.head == None:
+            self.head = Node(e)
+            self.size += 1
+            return
+        if node.next == None:
+            new_node = Node(e)
+            node.next = new_node
+            self.size += 1
+            return
+        self.append(node.next, e)
+
+    def preppend(self, e):  # Añadir nodos al comienzo
+        new_node = Node(e)
+        new_node.next = self.head
+        self.head = new_node
+
+    def delete_at_index(self, index):  # Eliminar nodos en un indice dado
+        if index > self.size - 1:
+            return
+        if index == 0:
+            self.head = self.head.next
+        else:
+            pos = 0
+            node = self.head
+            while pos != index - 1:
+                node = node.next
+                pos += 1
+            aux_node = node.next.next
+            node.next.next = None
+            node.next = aux_node
+
+
+
